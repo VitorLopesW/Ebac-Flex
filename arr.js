8b68752cca894cb934b6672e67d0c8764e96b148
VitorLopesW <67442259+VitorLopesW@users.noreply.github.com>
2023-11-23 21:11:36 -0300

    update

---
+++ arr.js
@@ -0,0 +1,245 @@
+export const arr = [
+    {
+      "img": "https://i0.wp.com/vooks.net/img/2019/06/Mario-Luigi-646x431.jpg?ssl=1",
+      "name": "Nintendo 3ds XL - Mario & Luigi: Dream Team",
+      "anunciante": "NinStore",
+      "console": "Nintendo 3DS",
+      "preço": 1099.99,
+      "promoNumber": 2,
+      "promoPreço": "769.99"
+    },
+    {
+      "img": "https://i0.wp.com/vooks.net/img/2019/06/Silver.jpg?ssl=1",
+      "name": "Nintendo 3ds XL - Silver Black",
+      "anunciante": "NinStore",
+      "console": "Nintendo 3DS",
+      "preço": 999.99,
+      "promoNumber": 6,
+      "promoPreço": "699.99"
+    },
+    {
+      "img": "https://i0.wp.com/vooks.net/img/2019/06/Animal-Crossing-New-Leaf.jpg?ssl=1",
+      "name": "Nintendo 3ds XL - Animal Crossing New Leaf",
+      "anunciante": "NinStore",
+      "console": "Nintendo 3DS",
+      "preço": 1299.99,
+      "promoNumber": 6,
+      "promoPreço": "909.99"
+    },
+    {
+      "img": "https://i0.wp.com/vooks.net/img/2019/06/Pokemon-XY-Red.jpg?ssl=1",
+      "name": "Nintendo 3ds XL - Pokemon X/Y (RED)",
+      "anunciante": "NinStore",
+      "console": "Nintendo 3DS",
+      "preço": 1599.99,
+      "promoNumber": 3,
+      "promoPreço": "1119.99"
+    },
+    {
+      "img": "https://i0.wp.com/vooks.net/img/2019/06/Monster-Hunter-X-Hunting-Life-646x334.jpg?ssl=1",
+      "name": "Nintendo 3ds XL - Monster Hunter X Hunting Life",
+      "anunciante": "NinStore",
+      "console": "Nintendo 3DS",
+      "preço": 3999.99,
+      "promoNumber": 1,
+      "promoPreço": "2799.99"
+    },
+    {
+      "img": "https://i0.wp.com/vooks.net/img/2019/06/Hyrule-646x348.jpg?ssl=1",
+      "name": "Nintendo 3ds XL - Hyrule",
+      "anunciante": "NinStore",
+      "console": "Nintendo 3DS",
+      "preço": 2999.99,
+      "promoNumber": 5,
+      "promoPreço": "2099.99"
+    },
+    {
+      "img": "https://i0.wp.com/vooks.net/img/2019/06/Electric-Blue.jpg?ssl=1",
+      "name": "Nintendo 2ds - Electric Blue",
+      "anunciante": "NinStore",
+      "console": "Nintendo 2DS",
+      "preço": 799.99,
+      "promoNumber": 6,
+      "promoPreço": "559.99"
+    },
+    {
+      "img": "https://i0.wp.com/vooks.net/img/2019/06/Crimson-Red.jpg?ssl=1",
+      "name": "Nintendo 2ds - Crimson Red",
+      "anunciante": "NinStore",
+      "console": "Nintendo 2DS",
+      "preço": 799.99,
+      "promoNumber": 1,
+      "promoPreço": "559.99"
+    },
+    {
+      "img": "https://i0.wp.com/vooks.net/img/2019/06/Transparent-Green-646x646.jpg?ssl=1",
+      "name": "Nintendo 2ds -Transparent Green",
+      "anunciante": "NinStore",
+      "console": "Nintendo 2DS",
+      "preço": 799.99,
+      "promoNumber": 4,
+      "promoPreço": "559.99"
+    },
+    {
+      "img": "/img/img-one.jpg",
+      "name": "Nintendo 3ds XL - Mario & Luigi: Dream Team USADO",
+      "anunciante": "João Lima",
+      "console": "Nintendo 3DS",
+      "preço": 899.99,
+      "promoNumber": 5,
+      "promoPreço": "629.99"
+    },
+    {
+      "img": "https://static1.thegamerimages.com/wordpress/wp-content/uploads/2021/07/Disney-Tsum-Tsum-Switch-System.jpg?q=50&fit=crop&w=963&dpr=1.5",
+      "name": "Nintendo Switch V2 - Disney Tsum Tsum",
+      "anunciante": "NinStore",
+      "console": "Nintendo Switch",
+      "preço": 4499.99,
+      "promoNumber": 1,
+      "promoPreço": "3149.99"
+    },
+    {
+      "img": "https://static1.thegamerimages.com/wordpress/wp-content/uploads/2021/07/Monster-Hunter-XX-Switch-System.jpg?q=50&fit=crop&w=963&dpr=1.5",
+      "name": "Nintendo Switch V2 - Monster Hunter XX",
+      "anunciante": "NinStore",
+      "console": "Nintendo Switch",
+      "preço": 2499.99,
+      "promoNumber": 6,
+      "promoPreço": "1749.99"
+    },
+    {
+      "img": "https://static1.thegamerimages.com/wordpress/wp-content/uploads/2021/07/Super-Smash-Bros-Ultimate-Switch-System.jpg?q=50&fit=crop&w=963&dpr=1.5",
+      "name": "Nintendo Switch V2 - Super Smash Bros Ultimate",
+      "anunciante": "NinStore",
+      "console": "Nintendo Switch",
+      "preço": 1999.99,
+      "promoNumber": 1,
+      "promoPreço": "1399.99"
+    },
+    {
+      "img": "/img/img-two.jpg",
+      "name": "Nintendo Switch Lite -Zacian Zamazenta USADO",
+      "anunciante": "Claudio Augusto",
+      "console": "Nintendo Switch",
+      "preço": 1599.99,
+      "promoNumber": 1,
+      "promoPreço": "1119.99"
+    },
+    {
+      "img": "/img/img-three.jpg",
+      "name": "Nintendo Switch OLED - Branco",
+      "anunciante": "NinStore",
+      "console": "Nintendo Switch",
+      "preço": 2199.99,
+      "promoNumber": 6,
+      "promoPreço": "1539.99"
+    },
+    {
+      "img": "/img/amiibo-1.webp",
+      "name": "Amiibo - Link BOTW",
+      "anunciante": "NinStore",
+      "console": "Amiibo",
+      "preço": 159.99,
+      "promoNumber": 4,
+      "promoPreço": "109.99"
+    },
+    {
+        "img": "/img/amiibo-3.webp",
+        "name": "Amiibo - Mega Man",
+        "anunciante": "NinStore",
+        "console": "Amiibo",
+        "preço": 159.99,
+        "promoNumber": 1,
+        "promoPreço": "109.99"
+      },
+      {
+        "img": "/img/amiibo-4.webp",
+        "name": "Amiibo - Ryu",
+        "anunciante": "NinStore",
+        "console": "Amiibo",
+        "preço": 159.99,
+        "promoNumber": 1,
+        "promoPreço": "109.99"
+      },
+      {
+        "img": "/img/amiibo-2.webp",
+        "name": "Amiibo - Zelda BOTW",
+        "anunciante": "NinStore",
+        "console": "Amiibo",
+        "preço": 159.99,
+        "promoNumber": 3,
+        "promoPreço": "109.99"
+      },
+      {
+        "img": "/img/amiibo-5.webp",
+        "name": "Amiibo - Zelda Wind Waker",
+        "anunciante": "NinStore",
+        "console": "Amiibo",
+        "preço": 159.99,
+        "promoNumber": 3,
+        "promoPreço": "109.99"
+      },
+      {
+        "img": "/img/amiibo-6.webp",
+        "name": "Amiibo - Cloud",
+        "anunciante": "NinStore",
+        "console": "Amiibo",
+        "preço": 159.99,
+        "promoNumber": 6,
+        "promoPreço": "109.99"
+      },
+      {
+        "img": "/img/amiibo-7.jpg",
+        "name": "Amiibo - Toon Link",
+        "anunciante": "NinStore",
+        "console": "Amiibo",
+        "preço": 159.99,
+        "promoNumber": 3,
+        "promoPreço": "109.99"
+      },
+      {
+        "img": "/img/amiibo-8.webp",
+        "name": "Amiibo - Pikachu",
+        "anunciante": "NinStore",
+        "console": "Amiibo",
+        "preço": 159.99,
+        "promoNumber": 5,
+        "promoPreço": "109.99"
+      },
+      {
+        "img": "/img/amiibo-9.webp",
+        "name": "Amiibo - Callie",
+        "anunciante": "NinStore",
+        "console": "Amiibo",
+        "preço": 159.99,
+        "promoNumber": 6,
+        "promoPreço": "109.99"
+      },
+      {
+        "img": "/img/amiibo-10.webp",
+        "name": "Amiibo - Pyra",
+        "anunciante": "NinStore",
+        "console": "Amiibo",
+        "preço": 159.99,
+        "promoNumber": 2,
+        "promoPreço": "109.99"
+      },
+      {
+        "img": "/img/amiibo-11.webp",
+        "name": "Amiibo - Inkling Yellow",
+        "anunciante": "NinStore",
+        "console": "Amiibo",
+        "preço": 159.99,
+        "promoNumber": 1,
+        "promoPreço": "109.99"
+      },
+      {
+        "img": "/img/amiibo-12.webp",
+        "name": "Amiibo - Kirby",
+        "anunciante": "NinStore",
+        "console": "Amiibo",
+        "preço": 159.99,
+        "promoNumber": 1,
+        "promoPreço": "109.99"
+      },
+  ]
--- main.js
+++ main.js
@@ -1,151 +1,83 @@
+import { arr } from './arr.js'
+const container = $('.container')
 
-const arr = [{
-    img: 'https://i0.wp.com/vooks.net/img/2019/06/Mario-Luigi-646x431.jpg?ssl=1',
-    name: 'Nintendo 3ds XL - Mario & Luigi: Dream Team',
-    anunciante: 'NinStore',
-    console: 'Nintendo 3DS',
-    preço: 1099.99,
-    },{
-        img: 'https://i0.wp.com/vooks.net/img/2019/06/Silver.jpg?ssl=1',
-        name: 'Nintendo 3ds XL - Silver Black',
-        anunciante: 'NinStore',
-        console: 'Nintendo 3DS',
-        preço: 999.99,
-    },{
-        img: 'https://i0.wp.com/vooks.net/img/2019/06/Animal-Crossing-New-Leaf.jpg?ssl=1',
-        name: 'Nintendo 3ds XL - Animal Crossing New Leaf',
-        anunciante: 'NinStore',
-        console: 'Nintendo 3DS',
-        preço: 1299.99,
-    },{
-        img: 'https://i0.wp.com/vooks.net/img/2019/06/Pokemon-XY-Red.jpg?ssl=1',
-        name: 'Nintendo 3ds XL - Pokemon X/Y (RED)',
-        anunciante: 'NinStore',
-        console: 'Nintendo 3DS',
-        preço: 1599.99,
-    },{
-        img: 'https://i0.wp.com/vooks.net/img/2019/06/Monster-Hunter-X-Hunting-Life-646x334.jpg?ssl=1',
-        name: 'Nintendo 3ds XL - Monster Hunter X Hunting Life',
-        anunciante: 'NinStore',
-        console: 'Nintendo 3DS',
-        'preço': 3999.99
-    },{
-        img: 'https://i0.wp.com/vooks.net/img/2019/06/Hyrule-646x348.jpg?ssl=1',
-        name: 'Nintendo 3ds XL - Hyrule',
-        anunciante: 'NinStore',
-        console: 'Nintendo 3DS',
-        'preço': 2999.99
-    },{
-        img: 'https://i0.wp.com/vooks.net/img/2019/06/Electric-Blue.jpg?ssl=1',
-        name: 'Nintendo 2ds - Electric Blue',
-        anunciante: 'NinStore',
-        console: 'Nintendo 2DS',
-        'preço': 799.99
-    },{
-        img: 'https://i0.wp.com/vooks.net/img/2019/06/Crimson-Red.jpg?ssl=1',
-        name: 'Nintendo 2ds - Crimson Red',
-        anunciante: 'NinStore',
-        console: 'Nintendo 2DS',
-        'preço': 799.99
-    },{
-        img: 'https://i0.wp.com/vooks.net/img/2019/06/Transparent-Green-646x646.jpg?ssl=1',
-        name: 'Nintendo 2ds -Transparent Green',
-        anunciante: 'NinStore',
-        console: 'Nintendo 2DS',
-        'preço': 799.99
-    },{
-        img: '/img/img-one.jpg',
-        name: 'Nintendo 3ds XL - Mario & Luigi: Dream Team USADO',
-        anunciante: 'João Lima',
-        console: 'Nintendo 3DS',
-        preço: 899.99,
-    },{
-        img: 'https://static1.thegamerimages.com/wordpress/wp-content/uploads/2021/07/Disney-Tsum-Tsum-Switch-System.jpg?q=50&fit=crop&w=963&dpr=1.5',
-        name: 'Nintendo Switch V2 - Disney Tsum Tsum',
-        anunciante: 'NinStore',
-        console: 'Nintendo Switch',
-        preço: 4499.99,
-    },{
-        img: 'https://static1.thegamerimages.com/wordpress/wp-content/uploads/2021/07/Monster-Hunter-XX-Switch-System.jpg?q=50&fit=crop&w=963&dpr=1.5',
-        name: 'Nintendo Switch V2 - Monster Hunter XX',
-        anunciante: 'NinStore',
-        console: 'Nintendo Switch',
-        preço: 2499.99,
-    },{
-        img: 'https://static1.thegamerimages.com/wordpress/wp-content/uploads/2021/07/Super-Smash-Bros-Ultimate-Switch-System.jpg?q=50&fit=crop&w=963&dpr=1.5',
-        name: 'Nintendo Switch V2 - Super Smash Bros Ultimate',
-        anunciante: 'NinStore',
-        console: 'Nintendo Switch',
-        preço: 1999.99,
-    },{
-        img: '/img/img-two.jpg',
-        name: 'Nintendo Switch Lite -Zacian Zamazenta USADO',
-        anunciante: 'Claudio Augusto',
-        console: 'Nintendo Switch',
-        preço: 1599.99,
-    },{
-        img: '/img/img-three.jpg',
-        name: 'Nintendo Switch OLED - Branco',
-        anunciante: 'NinStore',
-        console: 'Nintendo Switch',
-        preço: 2199.99,
-    }
-]
+function load(console){
+    container.html('')
+    let sideBarArr = []
+    const sideBar = $('.side-bar')
+    let sideBarHTML = '<div class="btn flex-centralize"><span class="material-symbols-outlined">menu</span> TODOS</div>'
+    sideBar.html(sideBarHTML)
+    arr.forEach((el) =>{
+            if(sideBarArr.includes(el.console) === false){
+                sideBarArr.push(el.console)
+                sideBar.append(`<div class='btn'>${el.console}</div>`)
+            }
+    })
+    const buttons = document.querySelectorAll('.btn');
 
-const content = document.querySelector('.content')
+    buttons.forEach((btn) => {
+        btn.addEventListener('click', (event) => {
+            if(event.target.innerHTML === '<span class="material-symbols-outlined">menu</span> TODOS') {
+                load(false)
+            } else {
+                load(event.target.innerHTML)
+            }
+        })
+    })
 
-arr.forEach((el) => {
     //
-    const newDiv = document.createElement('div')
-    newDiv.classList.add('container-product')
-    //
-    let [integer, decimal] = el.preço.toString().split('.')
-    newDiv.innerHTML = `
-    <div class='container-img flex-centralize '>
-        <img class='thumbnail' src='${el.img}'>
-    </div>
-        <div class='container-txt'>
-        <h2 class='titulo'>
-            ${el.name}
-        </h2>
-        <h3 class='anunciante'>
-            Anunciado Por: <b>${el.anunciante}</b>
-        </h3>
-        <a href='#'>${el.console}</a>
-        <h3 class='valor'>
-        <div class=small>R$</div><div class=big>${integer}</div><div class=small>${decimal}</div>
-        </h3>
-        <h3 class='parcelas'>
-            em até ${parcelas(el.preço)[0]}x de R$${parcelas(el.preço)[1]} sem juros
-        </h3>
-        <h3 class='frete'>
-            Frete Gratis
-        </h3>
-        <div class='btn-container flex-centralize'>
-            <input type='button' class='btn-comprar btn-carrinho' value='adicionar ao carrinho'>
-            <input type='button' class='btn-comprar btn-agora' value='Comprar agora'>
-        </div>
-    </div>
-    `
-    const buttons = newDiv.querySelectorAll('.btn-comprar');
-
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            button.classList.add('blink');
+    const arrFilter = console != false ? arr.filter((el) => el.console === console) : arr
+    arrFilter.forEach((el) =>{
+        const card = document.createElement('div')
+        card.classList.add('card')
+        const promo = dailyPromotion() == el.promoNumber ? 'red' : ''
+        const [integer, decimal] = String(el.preço).split('.')
+        card.innerHTML = `
+        
+            <div class='container-img flex-centralize'>
+                <img src='${el.img}'>
+            </div>
+            <div class='titulo'>${el.name}</div>
     
-            setTimeout(() => {
-                button.classList.remove('blink');
-            }, 1000);
-        });
-    });
+            <div class='anunciante'>
+                <h2>
+                    Anunciado Por: <b>${el.anunciante}</b>
+                </h2>
+            </div>
     
+            <a href='#' class='console'>${el.console}</a>
     
-    //
-
-    //append
-    content.insertBefore(newDiv, content.firstChild)
-})
-
+            <div class='preço ${promo} flex-centralize'>
+                <div class='small'>
+                    R$
+                </div>
+                <div class='big'>
+                    $${integer}
+                </div>
+                <div class='small'>
+                    ${decimal}
+                </div>
+            </div>
+    
+                        
+            <div class='parcelas'>
+                <p>em até ${parcelas(el.preço)[0]} de R$ ${parcelas(el.preço)[1]} sem juros</p>
+            </div>
+    
+            <div class='frete'>
+                <p>FRETE GRATIS</p>
+            </div>
+    
+            <div class='buttons'>
+                <input class='card-btn carrinho-btn' type='button' value='Adicionar Ao Carrinho '>
+                <input class='card-btn comprar-btn' type='button' value='Compre Agora'>
+            </div>
+    
+        `.trim()
+        container.append(card)
+    })
+    
+}
 
 function parcelas(num) {
     if (num >= 2000) return ['24', (num / 24).toFixed(2)]
@@ -157,35 +89,21 @@ function parcelas(num) {
     return ['2', (num / 2).toFixed(2)]
 }
 
-function resizeGrid() {
-    const win = $(window);
-    const width = win.width();
-    const content = $('.content');
-    const itemWidth = $('.container-product').width();
-    let calc = Math.floor(width / itemWidth);
-    if(calc == 1) calc++
-
-    if (width <= 450) {
-        $('.container-product').width(width).height('220px');
-        content.css("grid-template-columns", `100%`);
-    } else {
-        $('.container-product').width('200px').height('404px');
-
-        
-        content.css("grid-template-columns", `repeat(${calc}, 1fr)`);
-        
-    }
+function dailyPromotion(){
+    const date = new Date()
+    return date.getDay()
 }
 
-
 function onLoad(){
-    resizeGrid()
-    textHidden()
+    dailyPromotion()
+    load(false)
 }
 
 onLoad()
 
+//
 
+/*
 function textHidden(){
     const sobre = document.querySelector('#textSobreNos')
     const saibaMais = `<b id='btnText'  style='cursor: pointer'>Click Aqui para saber mais</b>`
@@ -202,9 +120,7 @@ function textHidden(){
 }
 
 
-
-
 $(window).on('resize orientationchange', function(el){
     resizeGrid()
 })
-
+*/
